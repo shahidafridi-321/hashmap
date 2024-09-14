441fe14c62809e3478f47ccd8c261b5ab94a3ff8
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-09-14 10:25:45 +0500

    added appriate comments

--- hash-map.js
+++ hash-map.js
@@ -1,11 +1,13 @@
+// A HASHMAP CLASS
 class HashMap {
 	constructor(size = 16) {
 		this.size = size;
-		this.buckets = Array(this.size)
+		this.buckets = Array(this.size) // creates an array of given size then fill the array with null so the map array method should iterate over it and returns an empty array for all the elements
 			.fill(null)
 			.map(() => []);
 	}
 
+	// take a string and generates and returns a hashCode for that string,the primeNumber is used to reduce the likely hood, the modulo operator is used to reduce a long hashCode to be inside the array index
 	hash(key) {
 		let hashCode = 0;
 		let primeNumber = 31;
@@ -15,6 +17,7 @@ class HashMap {
 		return hashCode;
 	}
 
+	// take key and a value,the key is converted to an index,at that index in buckets array ,if the key is already here then update the value otherwise put key value pair at that index
 	set(key, value) {
 		let index = this.hash(key);
 		let bucket = this.buckets[index];
@@ -26,7 +29,6 @@ class HashMap {
 		}
 		bucket.push([key, value]);
 	}
-
 }
 
 let hashMap = new HashMap();
